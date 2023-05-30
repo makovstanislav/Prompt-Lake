b8fa0ec5e17380dd6b55c5b0c2ece8b61186c92d
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-05-30 13:05:28 +0200

    Initial commit

---
+++ components/Feed.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard'
+
+const PromptCardList = ({ data }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([])
+
+  const fetchPosts = async() => {
+    const response = await fetch("/api/prompt")
+    const data = await response.json()
+    setAllPosts(data)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  return (
+    <section className='feed'>
+      <PromptCardList
+        data={allPosts}
+      />
+    </section>
+  )
+}
+
+export default Feed
