bdf725224a685fdf71a442fa590441262daddc2d
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-06-01 19:56:47 +0200

    Disable edge cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,7 +9,7 @@ export const GET = async (request) => {
         return new Response(JSON.stringify(prompts), { 
             status: 200,
             headers: {
-                'Cache-Control': 'no-cache'
+                'Cache-Control': 'no-store'
             }
         })
     } catch (error) {
