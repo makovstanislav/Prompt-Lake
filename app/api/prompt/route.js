3b482235a58d2887c0f7da96cd817d19a8eb1e2a
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-06-01 11:12:32 +0200

    Set cache control to 0

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,12 @@ export const GET = async (request) => {
         await connectToDB()
 
         const prompts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { 
+            status: 200,
+            headers: {
+                'Cache-Control': 's-maxage=0'
+            }
+        })
     } catch (error) {
         console.log(error)
         return new Response("Failed to fetch all prompts", { status: 500 })
