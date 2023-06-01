92dc51b6387fe1b3ddcc3595160a39dfe22d432b
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-06-01 12:18:14 +0200

    Update cache-control in prompt handler

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,7 +9,7 @@ export const GET = async (request) => {
         return new Response(JSON.stringify(prompts), { 
             status: 200,
             headers: {
-                'Cache-Control': 'no-store'
+                'Cache-Control': 's-maxage=1, stale-while-revalidate'
             }
         })
     } catch (error) {
