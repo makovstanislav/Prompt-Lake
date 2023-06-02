bf78e3c50856dc12a106785b59e345b585d55d86
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-06-02 14:09:47 +0200

    Add stale-while-revalidate Vercel cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,7 +9,7 @@ export const GET = async (request) => {
         return new Response(JSON.stringify(prompts), { 
             status: 200,
             headers: {
-                'Cache-Control': 'no-store'
+                'Cache-Control': 'stale-while-revalidate'
             }
         })
     } catch (error) {
