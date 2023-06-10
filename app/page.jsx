5945ffa306e0776324c4173289f187d25fbcd133
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-06-10 15:23:51 +0200

    Switch homepage to fully dynamic rendering

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Feed from "@components/Feed"
-export const revalidate = 0
+export const dynamic = "force-dynamic"
 
 export default async function Home() {
    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/prompt`, { 
